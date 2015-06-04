9f7173d48e4b197224961c48953f1a2701c5241a
megha jindal <megha.6sagi@gmail.com>
2015-06-04 15:03:45 +0200

    updated code

---
+++ app/src/main/java/fitness/com/fitness/Premium.java
@@ -0,0 +1,29 @@
+package fitness.com.fitness;
+
+public class Premium {
+
+	private String _id;
+	private String premiumValue;
+	private Period period;
+	
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String getPremiumValue() {
+		return premiumValue;
+	}
+	public void setPremiumValue(String premiumValue) {
+		this.premiumValue = premiumValue;
+	}
+	public Period getPeriod() {
+		return period;
+	}
+	public void setPeriod(Period period) {
+		this.period = period;
+	}
+	
+	
+}
