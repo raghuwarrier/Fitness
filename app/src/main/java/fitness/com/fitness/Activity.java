0d8a7d05415c9800f3b6b2bcf8e0a24f10199504
megha jindal <megha.6sagi@gmail.com>
2015-06-04 15:01:09 +0200

    updated code

---
+++ app/src/main/java/fitness/com/fitness/Activity.java
@@ -0,0 +1,36 @@
+package fitness.com.fitness;
+
+/**
+ * Created by meghajindal on 04/06/15.
+ */
+public class Activity {
+
+    private String type;
+    private String count;
+    private String unit;
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
---
+++ app/src/main/java/fitness/com/fitness/ActivityList.java
@@ -0,0 +1,20 @@
+package fitness.com.fitness;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityList {
+
+	List<Activity> activities = new ArrayList<Activity>();
+
+	public List<Activity> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(List<Activity> activities) {
+		this.activities = activities;
+	}
+
+	
+	
+}
