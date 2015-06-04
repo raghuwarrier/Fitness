95cd94aafe81f4f24bf1652ebb301ddba08f550f
megha jindal <megha.6sagi@gmail.com>
2015-06-04 15:01:34 +0200

    updated code

---
+++ app/src/main/java/fitness/com/fitness/Customer.java
@@ -0,0 +1,17 @@
+package fitness.com.fitness;
+
+/**
+ * Created by meghajindal on 30/05/15.
+ */
+public class Customer {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/fitness/com/fitness/Device.java
@@ -0,0 +1,19 @@
+package fitness.com.fitness;
+
+/**
+ * Created by meghajindal on 03/06/15.
+ */
+public class Device {
+
+    private String deviceId;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+
+}
